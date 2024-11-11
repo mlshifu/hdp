4e5ef1a96061012b905b4730353050afc156b7f6
mlshifu <uday_mahanthi@yahoo.com>
2024-11-12 04:29:43 +0530

    test
    test scripts

--- sftp.sh
+++ sftp.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# Variables
-REMOTE_USER="your_username"      # Replace with your remote username
-REMOTE_HOST="remote.server.com"   # Replace with your remote server address
-REMOTE_DIR="/path/to/remote/dir"  # Replace with the remote directory where you want to upload the file
-LOCAL_FILE="/path/to/local/file"   # Replace with the local file you want to upload
-
-# Use SFTP to transfer the file
-sftp "$REMOTE_USER@$REMOTE_HOST" << EOF
-put "$LOCAL_FILE" "$REMOTE_DIR"
-bye
-EOF
-
-echo "File $LOCAL_FILE has been transferred to $REMOTE_HOST:$REMOTE_DIR"
+print(hello)
